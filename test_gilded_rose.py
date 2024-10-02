867a0ef91dda2f3cbfa45a9e9f750732894b1e59
Itsc2y <c2ymonica@gmail.com>
2024-10-01 20:38:06 -0700

    add failed test cases

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,6 +11,32 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
 
+    def test_vest_item_should_decrease_after_one_day(self):
+        vest = "+5 Dexterity Vest"
+        items = [Item(vest, 1, 2)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+     
+        assert gr.items[0] == Item(vest, 0, 3)
+    
+    def test_brie_item_should_increase_after_one_day(self):
+        brie = "Aged Brie"
+        items = [Item(brie, 2, 0)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        assert gr.items[0] == Item(brie, 1, 0)
+
+    def test_quality_should_no_more_than_fifty(self):
+        brie = "Aged Brie"
+        items = [Item(brie, 2, 50)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        assert gr.items[0] == Item(brie, 1, 51)
 
 if __name__ == '__main__':
     unittest.main()
